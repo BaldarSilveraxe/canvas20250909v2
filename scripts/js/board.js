73b34d871ae7c7d6fe8a01f18bc0d1f2d75a9410
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-10 06:47:59 -0400

    Update board.js

--- scripts/js/board.js
+++ scripts/js/board.js
@@ -3,7 +3,27 @@ import addGridGroup from './features/board/grid.js';
 import DEFAULT_CONFIG from './configuration/board/DEFAULT_CONFIG.js?d=2&ddd=1';
 
 const board = (() => {
-    console.log('board');
+   const create = (kCanvas) => {
+        console.log('here');
+
+        // Public API
+        return {
+            //stage,
+            //layers: {
+            //    world: layers.worldLayer,
+            //    items: layers.itemsLayer
+            //},
+            //groups: {
+            //    world: groups.worldGroup,
+            //    items: groups.itemsGroup,
+            //    grid_group: worldContent.gridGroup
+            //},
+            //camera: cameraSystem.camera,
+            //centerWorld: cameraSystem.centerWorld,
+            //setGridVisible: keyboardHandlers.setGridVisible,
+            //toggleGrid: keyboardHandlers.toggleGrid
+        };
+    };
     return { //create };
 })();
 
