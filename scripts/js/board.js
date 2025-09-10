bdd018ba0544106dcb92b00c9e2570011132ed66
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-10 06:41:25 -0400

    Create board.js

---
+++ scripts/js/board.js
@@ -0,0 +1,10 @@
+import Konva from 'https://esm.sh/konva@9';
+import addGridGroup from './features/board/grid.js';
+import DEFAULT_CONFIG from './configuration/board/DEFAULT_CONFIG.js?d=2&ddd=1';
+
+const board = (() => {
+    console.log('board');
+    return { //create };
+})();
+
+export { board };
