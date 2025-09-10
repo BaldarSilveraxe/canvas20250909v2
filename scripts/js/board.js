73a790502f1bf04bec8922e7d7793d20eb462ad3
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-10 07:02:15 -0400

    Update board.js

--- scripts/js/board.js
+++ scripts/js/board.js
@@ -3,18 +3,22 @@ import Konva from 'https://esm.sh/konva@9';
 //import DEFAULT_CONFIG from './configuration/board/DEFAULT_CONFIG.js?d=2&ddd=1';
 
 const board = (() => {
+    
+    const stageBuilder = (cnvs) => {
+        const kCanvasContainer = (typeof cnvs === 'string') ? document.getElementById(cnvs) : cnvs;
+        if (!kCanvasContainer) {
+            throw new Error('board.create: container not found');
+        }
+        return new Konva.Stage({
+            container: kCanvasContainer,
+            width: kCanvasContainer.clientWidth,
+            height: kCanvasContainer.clientHeight,
+        });
+    };
+    
     const create = (kCanvas) => {
-        const stage = () => {
-            const kCanvasContainer = (typeof kCanvas === 'string') ? document.getElementById(kCanvas) : kCanvas;
-            if (!kCanvasContainer) {
-                throw new Error('board.create: container not found');
-            }
-            return new Konva.Stage({
-                container: kCanvasContainer,
-                width: kCanvasContainer.clientWidth,
-                height: kCanvasContainer.clientHeight,
-            });
-        };
+        const stage = stageBuilder(cnvs)
+
 
         console.log(stage);
 
