fbde24a2c2bc43c8d28fc6de7628d806ff12bd0e
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-10 06:52:26 -0400

    Update board.js

--- scripts/js/board.js
+++ scripts/js/board.js
@@ -1,6 +1,6 @@
 import Konva from 'https://esm.sh/konva@9';
-import addGridGroup from './features/board/grid.js';
-import DEFAULT_CONFIG from './configuration/board/DEFAULT_CONFIG.js?d=2&ddd=1';
+//import addGridGroup from './features/board/grid.js';
+//import DEFAULT_CONFIG from './configuration/board/DEFAULT_CONFIG.js?d=2&ddd=1';
 
 const board = (() => {
     const create = (kCanvas) => {
