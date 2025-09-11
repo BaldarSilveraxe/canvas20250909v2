c762fc1f23aea04cc1059929cda78a574f5adefb
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-11 19:04:07 -0400

    Update board.js

--- scripts/js/board.js
+++ scripts/js/board.js
@@ -44,7 +44,7 @@ const board = (() => {
             colorMajor: '#000000',
             strokeWidthMinor: 1,
             strokeWidthMajor: 2,
-            minorLine: 150,
+            minorLine: 10,
             majorLineEvery: 5
         },
         zoom: {
