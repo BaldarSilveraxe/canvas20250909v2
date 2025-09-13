d48d1709539d3e2a2708812b572bf4ec930bb9cf
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-13 11:11:45 -0400

    Update board.js

--- scripts/js/board.js
+++ scripts/js/board.js
@@ -394,7 +394,16 @@ const teardown = () => {
         };
 
 const makeBoardControls = () => {
-
+  const bg = new Konva.Circle({
+    radius: 24,
+    fill: color,
+    shadowColor: 'black',
+    shadowBlur: 16,
+    shadowOffsetY: Math.round(size * 0.14), // elevation feel
+    shadowOpacity: 0.3
+  });
+const targetGroup = getNodeByName('group-ui-pseudoLayer-main');
+    targetGroup.add(bg);
 };
 
         const makeGrid = () => {
