2637113c2302cff2ce280af07d2d49401dc8c4b2
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-14 04:56:11 -0400

    Update build.js

--- scripts/js/build.js
+++ scripts/js/build.js
@@ -1,7 +1,7 @@
 import Konva from 'https://esm.sh/konva@9';
 
 export const build = {
-  setStageLayersGroups(kCanvas, canvasState) {
+  setStageLayersGroups(kCanvas, canvasState, CONFIG) {
     console.log(CONFIG);
     // No Konva. Just mutate the same object you passed in.
     canvasState.stage = 'Hello';   // or whatever you want here
