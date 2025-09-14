9c2da541d0921254cd0df8595e6be35cb41b1be9
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-14 10:08:59 -0400

    Update utilities.js

--- scripts/js/utilities/utilities.js
+++ scripts/js/utilities/utilities.js
@@ -1,16 +1,7 @@
-export const const utility = () => {
+export const utility = () => {
 
-    const addNode = ({ stateType, name, konvaNode }) => {
-        const validStateTypes = new Set(['layers', 'groups', 'shapes']);
-        if (!validStateTypes.has(stateType)) {
-            console.error(`Invalid stateType: ${stateType}. Must be one of layers, groups, or shapes.`);
-            return null;
-        }
-        const id = crypto.randomUUID();
-        konvaNode.id(id);
-        canvasState[stateType][id] = konvaNode;
-        canvasState.index[name] = id;
-        return { id, node: konvaNode };
+    const addNode = () => {
+
     };
 
     const otherFucntion = () => {
