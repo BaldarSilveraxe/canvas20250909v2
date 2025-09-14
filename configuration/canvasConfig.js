80bf500d3b714a410f4023c00b3927b8e3b2f5b2
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-14 15:05:13 -0400

    Update canvasConfig.js

--- configuration/canvasConfig.js
+++ configuration/canvasConfig.js
@@ -1,5 +1,8 @@
 // config.js
 export const CONFIG = {
+    stage: null,
+    indexId: {},
+    IndexName: {},
     build: {
         stage: {
             name: '_stage',
@@ -12,58 +15,53 @@ export const CONFIG = {
                 border: 0
             },
         },
-        indexId: {},
-        IndexName: {},
         layers: ['layer-world', 'layer-items', 'layer-ui'],
-    pseudoLayers: {
-      world: {
-        group: 'group-world-pseudoLayer-camera-wrap',
-        pseudos: [
-          'group-world-pseudoLayer-background',
-          'group-world-pseudoLayer-grid',
-        ],
-      },
-      items: {
-        group: 'group-items-pseudoLayer-camera-wrap',
-        pseudos: [
-          'group-items-pseudoLayer-z-0',
-          'group-items-pseudoLayer-z-10',
-          'group-items-pseudoLayer-z-20',
-          'group-items-pseudoLayer-z-30',
-          'group-items-pseudoLayer-z-40',
-        ],
-      },
+        pseudoLayers: {
+            world: {
+                group: 'group-world-pseudoLayer-camera-wrap',
+                pseudos: ['group-world-pseudoLayer-background', 'group-world-pseudoLayer-grid'],
+            },
+            items: {
+                group: 'group-items-pseudoLayer-camera-wrap',
+                pseudos: [
+                    'group-items-pseudoLayer-z-0',
+                    'group-items-pseudoLayer-z-10',
+                    'group-items-pseudoLayer-z-20',
+                    'group-items-pseudoLayer-z-30',
+                    'group-items-pseudoLayer-z-40',
+                ],
+            },
+        },
+        cameraWraps: {
+            worldCamera: 'group-world-pseudoLayer-camera-wrap',
+            itemsCamera: 'group-items-pseudoLayer-camera-wrap',
+        },
+        RESERVED_NAMES: new Set([
+            '_stage',
+            'layer-world',
+            'layer-items',
+            'layer-ui',
+            'group-world-pseudoLayer-camera-wrap',
+            'group-world-pseudoLayer-background',
+            'group-world-pseudoLayer-grid',
+            'group-items-pseudoLayer-camera-wrap',
+            'group-items-pseudoLayer-z-0',
+            'group-items-pseudoLayer-z-10',
+            'group-items-pseudoLayer-z-20',
+            'group-items-pseudoLayer-z-30',
+            'group-items-pseudoLayer-z-40',
+            'group-ui-pseudoLayer-main',
+            'shape-pseudoLayer-background-rect',
+            'group-world-grid',
+        ]),
+        world: {
+            x: 0,
+            y: 0,
+            name: 'shape-pseudoLayer-background-rect',
+            width: 6000,
+            height: 6000,
+            fill: '#000000',
+            listening: true,
+        },
     },
-    cameraWraps: {
-      worldCamera: 'group-world-pseudoLayer-camera-wrap',
-      itemsCamera: 'group-items-pseudoLayer-camera-wrap',
-    },
-    RESERVED_NAMES: new Set([
-      '_stage',
-      'layer-world',
-      'layer-items',
-      'layer-ui',
-      'group-world-pseudoLayer-camera-wrap',
-      'group-world-pseudoLayer-background',
-      'group-world-pseudoLayer-grid',
-      'group-items-pseudoLayer-camera-wrap',
-      'group-items-pseudoLayer-z-0',
-      'group-items-pseudoLayer-z-10',
-      'group-items-pseudoLayer-z-20',
-      'group-items-pseudoLayer-z-30',
-      'group-items-pseudoLayer-z-40',
-      'group-ui-pseudoLayer-main',
-      'shape-pseudoLayer-background-rect',
-      'group-world-grid',
-    ]),
-    world: {
-      x: 0,
-      y: 0,
-      name: 'shape-pseudoLayer-background-rect',
-      width: 6000,
-      height: 6000,
-      fill: '#000000',
-      listening: true,
-    },
-  },
 };
