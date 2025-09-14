2cc685825742f1c02a66253475be9e3d37112d2f
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-14 04:50:16 -0400

    Update main.js

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -11,9 +11,50 @@ const canvasState = {
 };
 
 const config = {
-  build: {
-
-  }
+    build: {
+        layers: ['world', 'items', 'ui'],
+        pseudoLayers: {
+            world: {
+                group: 'group-world-pseudoLayer-camera-wrap',
+                pseudos: ['group-world-pseudoLayer-background', 'group-world-pseudoLayer-grid']
+            },
+            items: {
+                group: 'group-items-pseudoLayer-camera-wrap',
+                pseudos: ['group-items-pseudoLayer-z-0', 'group-items-pseudoLayer-z-10', 'group-items-pseudoLayer-z-20', 'group-items-pseudoLayer-z-30', 'group-items-pseudoLayer-z-40']
+            },
+        },
+        cameraWraps: {
+            worldCamera: 'group-world-pseudoLayer-camera-wrap', 
+            itemsCamera: 'group-items-pseudoLayer-camera-wrap'
+        },
+        RESERVED_NAMES: new Set([
+            '_stage',
+            'layer-world',
+            'layer-items',
+            'layer-ui',
+            'group-world-pseudoLayer-camera-wrap',
+            'group-world-pseudoLayer-background',
+            'group-world-pseudoLayer-grid',
+            'group-items-pseudoLayer-camera-wrap',
+            'group-items-pseudoLayer-z-0',
+            'group-items-pseudoLayer-z-10',
+            'group-items-pseudoLayer-z-20',
+            'group-items-pseudoLayer-z-30',
+            'group-items-pseudoLayer-z-40',
+            'group-ui-pseudoLayer-main',
+            'shape-pseudoLayer-background-rect',
+            'group-world-grid'
+        ]),
+        world: {
+            x: 0,
+            y: 0,
+            name: 'shape-pseudoLayer-background-rect',
+            width: 6000,
+            height: 6000,
+            fill: '#000000',
+            listening: true
+        },
+     };
 
 };
 
