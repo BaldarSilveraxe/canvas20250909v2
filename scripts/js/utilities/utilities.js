f134928df4cf43d9539573d333e2b9308e4b436d
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-14 10:18:49 -0400

    Update utilities.js

--- scripts/js/utilities/utilities.js
+++ scripts/js/utilities/utilities.js
@@ -1,4 +1,8 @@
-export const createUtility = (deps = {}) => {
+export const createUtility = ({ state, config }) => {
+  // state is mutable (instance data)
+  let s = state;
+  const cfg = deepFreeze(structuredClone(config)); // or just config if you trust callers
+  
   const addNode = () => {};
   const otherFunction = () => {};
 
