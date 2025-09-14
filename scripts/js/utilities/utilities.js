250bb8d9225d4e10342773697584d748e37cddd4
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-14 16:23:22 -0400

    Update utilities.js

--- scripts/js/utilities/utilities.js
+++ scripts/js/utilities/utilities.js
@@ -2,15 +2,18 @@ export const createUtility = ({ state, config }) => {
     // state is mutable (instance data)
     let s = state;
     const cfg = config;
-  
+
+    const addReserveName = (string) => {
+        if (!s.reservedName.has(string) {
+            s.add(string);
+        }
+    };
     const addNode = ({ name, id, konvaNode }) => {
         s.stage.add(konvaNode);
         s.indexId[id] = name;
         s.indexName[name] = id;
         return { node: konvaNode, id };
     };
-    
-    const otherFunction = () => {};
 
-    return { addNode, otherFunction };
+    return { addReserveName, addNode};
 };
