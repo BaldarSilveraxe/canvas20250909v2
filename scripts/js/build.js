9a4c3dcf6ae37bfa953876e674e08c8fe3c67219
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-13 20:59:26 -0400

    Update build.js

--- scripts/js/build.js
+++ scripts/js/build.js
@@ -1,9 +1,20 @@
 import Konva from 'https://esm.sh/konva@9';
 
-const setStageLayersGroups = ((kCanvas, canvasState) => {
+const build = ((kCanvas, canvasState) => {
         
         console.log(canvasState);
         console.log(kCanvas);
+        
+const setStageLayersGroups  = (kCanvas) => {
+
+        console.log('finished');
+        console.log(canvasState);
+
+        // Enhanced Public API
+        return {
+            stage: canvasState.stage
+        };
+    };
         
 return {
         //create
