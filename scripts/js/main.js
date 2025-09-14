346d67a27038f42d09c03ed05e7682861a1e146a
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-13 20:47:06 -0400

    Update main.js

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -1,8 +1,18 @@
-import { board } from './board.js';
+//import { board } from './board.js';
+import { board } from './build.js';
+
+const canvasState = {
+  stage: null,
+  layers: {},
+  groups: {},
+  shapes: {},
+  index: {},
+  resizeHandler: null, // Store for cleanup
+};
 
 // Utility function
 const getEl = (id) => document.getElementById(id);
 
 const kCanvas = getEl('konva-container');
 
-const newBoard = board.create(kCanvas);
+//const newBoard = board.create(kCanvas);
