29dc9210c2d5a2f5f97ea82deb0da04f26c5ee16
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-13 20:42:23 -0400

    Update build.js

--- scripts/js/build.js
+++ scripts/js/build.js
@@ -1,9 +1,17 @@
 import Konva from 'https://esm.sh/konva@9';
 
 const setStageLayersGroupKeyItems = (() => {
+        const canvasState = {
+                stage: null,
+                layers: {},
+                groups: {},
+                shapes: {},
+                index: {},
+                resizeHandler: null, // Store for cleanup
+            };
         console.log('newbuild');
 
 return {
-        create
+        //create
     };
 })();
