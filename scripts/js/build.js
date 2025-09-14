1932fcc4280eed29b5e632b6f0e4c90e21f0a424
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-14 08:00:57 -0400

    Update build.js

--- scripts/js/build.js
+++ scripts/js/build.js
@@ -1,15 +1,31 @@
+// Helper functions
+const makeStage = (el, state, config) => {
+    // Create Konva stage
+
+};
+
+const makeLayers = (state, config) => {
+
+};
+
+const teardown = (state) => {
+    if (state.stage) {
+        state.stage.destroy();
+        state.stage = null;
+    }
+    // Clear other state
+    Object.keys(state.layers).forEach(key => delete state.layers[key]);
+    Object.keys(state.groups).forEach(key => delete state.groups[key]);
+    Object.keys(state.shapes).forEach(key => delete state.shapes[key]);
+};
+
 export const build = {
     /**
-    * @param {Object} opts
-    * @param {HTMLElement} opts.htmlContainer
-    * @param {Object} opts.state
-    * @param {Object} [opts.config]
-    */
-
-    const makeStage = (el, state, config) => {
-        // Create Konva stage
-    };
-
+     * @param {Object} opts
+     * @param {HTMLElement} opts.htmlContainer
+     * @param {Object} opts.state
+     * @param {Object} [opts.config]
+     */
     setStageLayersGroups(opts = {}) {
         const {
             htmlContainer,
@@ -19,20 +35,29 @@ export const build = {
         
         if (!htmlContainer) throw new Error('setStageLayersGroups: htmlContainer is required');
         if (!state) throw new Error('setStageLayersGroups: state is required');
-
+        if (!config) throw new Error('setStageLayersGroups: config is required');
+        
         try {
             makeStage(htmlContainer, state, config);
-            //if (!(canvasState.stage instanceof Konva.Stage)) {
-            //    throw new Error('board.create: stage not created');
-            //}
-            //makeLayers();
-
-            //canvasState.stage.getLayers().forEach(l => l.batchDraw());
+            
+            if (!state.stage) {
+                throw new Error('setStageLayersGroups: stage not created');
+            }
+            
+            makeLayers(state, config);
+            
+            // Batch draw all layers
+            state.stage.getLayers().forEach(layer => layer.batchDraw());
+            
         } catch (err) {
-            //teardown();
+            teardown(state);
             throw err;
         }
-
-        return {  };
+        
+        return {
+            //stage: state.stage,
+            //layers: state.layers,
+            // Add other API methods as needed
+        };
     }
 };
