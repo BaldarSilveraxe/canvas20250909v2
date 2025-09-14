6ab30d965f7ae5291854752e5efe026148ae72ce
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-14 11:11:34 -0400

    Update utilities.js

--- scripts/js/utilities/utilities.js
+++ scripts/js/utilities/utilities.js
@@ -1,10 +1,13 @@
 export const createUtility = ({ state, config }) => {
-  // state is mutable (instance data)
-  let s = state;
-  const cfg = config;
+    // state is mutable (instance data)
+    let s = state;
+    const cfg = config;
   
-  const addNode = () => {};
-  const otherFunction = () => {};
+    const addNode = () => {
+        const id = crypto.randomUUID();
+        konvaNode.id(id);  
+    };
+    const otherFunction = () => {};
 
-  return { addNode, otherFunction };
+    return { addNode, otherFunction };
 };
