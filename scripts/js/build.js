1faba2526405a049c2deb155c82ad0c7f7deac6f
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-14 05:33:26 -0400

    Update build.js

--- scripts/js/build.js
+++ scripts/js/build.js
@@ -1,11 +1,24 @@
-import Konva from 'https://esm.sh/konva@9';
-
 export const build = {
-  setStageLayersGroups(kCanvas, canvasState, CONFIG) {
-    console.log(CONFIG);
-    // No Konva. Just mutate the same object you passed in.
-    canvasState.stage = 'Hello';   // or whatever you want here
-    const cats = 'cats';
-    return { cats };
-  }
+    /**
+    * @param {Object} opts
+    * @param {HTMLElement} opts.htmlContainer
+    * @param {Object} opts.state
+    * @param {Object} [opts.config]
+    */
+    setStageLayersGroups(opts = {}) {
+        const {
+            htmlContainer,
+            state,
+            config,
+        } = opts;
+        
+        if (!htmlContainer) throw new Error('setStageLayersGroups: htmlContainer is required');
+        if (!state) throw new Error('setStageLayersGroups: state is required');
+
+        console.log(CONFIG);
+
+        canvasState.stage = 'Hello';   // or whatever you want here
+        const cats = 'cats';
+        return { cats };
+    }
 };
