f6be63234214a402fd0b91e2bd0d507208e652e5
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-15 05:36:32 -0400

    Update build.js

--- scripts/js/build.js
+++ scripts/js/build.js
@@ -68,10 +68,9 @@ const makeCameraWrappers = (state, config, util) => {
 const makePseudoLayers = (state, config, util) => {
     let targetGroup, genId, kObj, node;
     Object.keys(config.build.pseudoLayers).forEach(key => {
-        console.log(config.build.pseudoLayers[key])
         if (config.build.layers[key] &&
-            config.build.layers[key].layerName) {
-            targetGroup = state.stage.findOne(`#${state.indexName[config.build.layers[key].layerName]}`);
+            config.build.layers[key].group) {
+            targetGroup = state.stage.findOne(`#${state.indexName[config.build.layers[key].group]}`);
             if (!targetGroup) throw new Error('[makePseudoLayers] target group not found');
             config.build.pseudoLayers[key].pseudos.forEach(name => {
                 genId = getId();
