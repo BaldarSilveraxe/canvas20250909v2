f376624cdcff8a2c78ff8eba440dd604ef01eb0d
BaldarSilveraxe <stephen.w.shomo@gmail.com>
2025-09-15 05:35:32 -0400

    Update build.js

--- scripts/js/build.js
+++ scripts/js/build.js
@@ -68,7 +68,7 @@ const makeCameraWrappers = (state, config, util) => {
 const makePseudoLayers = (state, config, util) => {
     let targetGroup, genId, kObj, node;
     Object.keys(config.build.pseudoLayers).forEach(key => {
-        console.log(config.build.pseudoLayers[key].group)
+        console.log(config.build.pseudoLayers[key])
         if (config.build.layers[key] &&
             config.build.layers[key].layerName) {
             targetGroup = state.stage.findOne(`#${state.indexName[config.build.layers[key].layerName]}`);
